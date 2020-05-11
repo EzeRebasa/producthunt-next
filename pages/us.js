0d24c1da576c68b3c715091bf48c0091110a5e47
Ezequiel <ezequielrebasa@gmail.com>
2020-05-11 20:10:14 -0300

    component Header and layout us added

---
+++ pages/us.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Us = () => (
+  <div>
+    <Layout>
+    <h1> Us </h1>
+    </Layout>
+  </div>
+);
+
+export default Us;
