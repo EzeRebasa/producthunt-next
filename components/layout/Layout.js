3329391b945ea5fee6cb6c7b2e7d54dec1711d16
Ezequiel <ezequielrebasa@gmail.com>
2020-05-12 14:02:17 -0300

    folder img added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,85 @@
 import React from "react";
-import Link from 'next/link';
-import Header from './Header';
+import Header from "./Header";
+import { Global, css } from "@emotion/core";
+import Head from "next/head";
 
 const Layout = (props) => {
   return (
     <>
+      <Global
+        styles={css`
+          :root {
+            --grey: #3d3d3d;
+            --grey2: #6f6f6f;
+            --orange: #da552f;
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.6rem;
+            line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1, 5;
+          }
+
+          h1,h2 {
+            font-family: 'Roboto Slab', serif;
+            font-weight: 700;
+          }
+
+          h3 {
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
+
+      <Head>
+        <html lang="en" />
+        <title> Product Hunt Firebase and Next.js </title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+          integrity="sha256-WAgYcAck1C1/zEl5sBl5cfyhxtLgKGdpI3oKyJffVRI="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;1,700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="/static/css/app.css" rel="stylesheet" />
+        />
+
+      </Head>
+
       <Header />
-      
+
       <main>{props.children}</main>
     </>
   );
