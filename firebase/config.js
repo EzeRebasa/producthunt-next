0efc8d5aa35125c34de90eb6d0d9cbe16ea81cef
Ezequiel <ezequielrebasa@gmail.com>
2020-05-12 21:09:58 -0300

    firebase added

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-hg8u5aiswzBjhpkQeTlWx0dcsyhbLXk",
+    authDomain: "product-hunt-91a9f.firebaseapp.com",
+    databaseURL: "https://product-hunt-91a9f.firebaseio.com",
+    projectId: "product-hunt-91a9f",
+    storageBucket: "product-hunt-91a9f.appspot.com",
+    messagingSenderId: "463801546015",
+    appId: "1:463801546015:web:e565a0dd5bce255ecb6c2a"
+  };
+
+  export default firebaseConfig;
---
+++ firebase/context.js
@@ -0,0 +1,6 @@
+// All data will be available in the app
+import React, {createContext} from 'react';
+
+const FirebaseContext = createContext(null);
+
+export default FirebaseContext;
